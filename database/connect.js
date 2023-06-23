3dfc609fc8d59def3697cc5dd9f560f58884884e
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2023-06-23 16:27:45 -0400

    Configurando la db

--- database/connect.js
+++ database/connect.js
@@ -1,15 +1,11 @@
 const mysql = require("mysql");
 require("dotenv").config()
-const connect = mysql.createConnection({
+const connect = mysql.createPool({
     database:process.env.database,
     password: process.env.passwordDatabase,
     host: process.env.DB_host,
-    user: process.env.user_db
+    user: process.env.user_db,
 })
 
-connect.connect((error)=>{
-    if(error) console.log("No se pudo conectar a la DB...",error.sqlMessage)
-    else(console.log("conexion exitosa a la base de datos"))
-})
 
 module.exports = connect
